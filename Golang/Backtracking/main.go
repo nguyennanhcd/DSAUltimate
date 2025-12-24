1994985f8d7a7a89955f005a38408e4a22dfebe5
nguyennanhcd <anh487303@gmail.com>
2025-12-24 20:20:45 +0700

    done

--- Golang/Backtracking/main.go
+++ Golang/Backtracking/main.go
@@ -1,1 +1,11 @@
 package main
+
+import "fmt"
+
+func main() {
+	nums1 := []int{1, 4}
+	fmt.Println("Permutations of", nums1, "are:", permute(nums1))
+
+	nums2 := []int{1, 4, 5}
+	fmt.Println("Permutations of", nums2, "are:", permute(nums2))
+}
--- Golang/Backtracking/permutation.go
+++ Golang/Backtracking/permutation.go
@@ -15,3 +15,24 @@ nums = [1,4,5]
 Output: [[1,4,5],[1,5,4],[4,1,5],[4,5,1],[5,1,4],[5,4,1]]
 */
 package main
+
+func permute(nums []int) [][]int {
+	var result [][]int
+	var backtrack func(start int)
+	backtrack = func(start int) {
+		if start == len(nums)-1 {
+			// Make a copy of nums and append to result
+			perm := make([]int, len(nums))
+			copy(perm, nums)
+			result = append(result, perm)
+			return
+		}
+		for i := start; i < len(nums); i++ {
+			nums[start], nums[i] = nums[i], nums[start] // Swap
+			backtrack(start + 1)                        // Recurse
+			nums[start], nums[i] = nums[i], nums[start] // Backtrack (swap back)
+		}
+	}
+	backtrack(0)
+	return result
+}
